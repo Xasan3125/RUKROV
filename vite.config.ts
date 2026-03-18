49625a20222caff5183da5d18d4820babeae3de0
Maxim Fesan <maxim.fesan@gmail.com>
2026-03-18 22:28:43 +0300

    Update dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+    plugins: [vue(), tailwindcss()],
+  base:'./',
+    resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "src"),
+        }
+    }
+})
