8a4a529f4515e8559c2394ccd0978f47da7a4b06
Maxim Fesan <maxim.fesan@gmail.com>
2026-03-23 22:07:26 +0300

    fix

--- src/components/AppProduct/AppProductCard/AppProductCard.vue
+++ src/components/AppProduct/AppProductCard/AppProductCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex  flex-col items-center w-full lg:h-[500px] rounded-xl shadow-[0px_0px_26px_8px_rgba(34,60,80,0.08)] ">
+  <div class="flex p-4 flex-col items-center w-full lg:h-[500px] rounded-xl  shadow-[0px_0px_26px_8px_rgba(34,60,80,0.08)] ">
     <img class="h-80 object-cover" :src="image" :alt="title">
     <h5 class="text-lg font-bold mb-2">{{title}}</h5>
     <p class="text-sm text-gray-500 text-center mb-4  md:w-[363px]">{{description}}</p>
